8f04e03e8656eaf0b32762399a0d4a76884e4885
msallemmahdi <msalem.mehdi@gmail.com>
2024-10-14 18:20:44 +0200

    user input to display exercice specifics

--- app.py
+++ app.py
@@ -2,10 +2,11 @@
 
 import duckdb
 import streamlit as st
+import ast
 
 con = duckdb.connect(database="data/exercices_sql_tables.duckdb", read_only=False)
 
-#solution_df = duckdb.sql(ANSWER_STR).df()
+# solution_df = duckdb.sql(ANSWER_STR).df()
 
 with st.sidebar:
     theme = st.selectbox(
@@ -16,15 +17,16 @@ with st.sidebar:
     )
     st.write("You selected", theme)
 
-    exercise = con.execute(f"SELECT * from memory_state where theme = '{theme}'")
+    exercise = con.execute(f"SELECT * from memory_state where theme = '{theme}'").df()
     st.write(exercise)
+
 st.header("enter your code")
 query = st.text_area(label="votre code sql ici", key="user_input")
 
 
-#if query:
-#    result = duckdb.sql(query).df()
-#    st.dataframe(result)
+if query:
+    result = con.execute(query).df()
+    st.dataframe(result)
 
 #    if len(result.columns) != len(solution_df.columns):
 #        st.write("Some Columns are missing")
@@ -42,15 +44,26 @@ query = st.text_area(label="votre code sql ici", key="user_input")
 #        )
 
 
-#tab2, tab3 = st.tabs(["Tables", "solution_df"])
+tab2, tab3 = st.tabs(["Tables", "solution_df"])
 
-#with tab2:
-#    st.write("table : beverages")
-#    st.dataframe(beverages)
-#    st.write("table: food_items")
-#    st.dataframe(food_items)
+with tab2:
+    #st.write(exercise.loc[0, "tables"])
+    exercise_tables = ast.literal_eval(exercise.loc[0, "tables"])
+    for table in exercise_tables:
+        st.write(f"table: {table}")
+        df_table = con.execute(f"SELECT * from {table}").df()
+        st.dataframe(df_table)
+#st.write("table: food_items")
+#st.dataframe(food_items)
 #    st.write("expected")
 #    st.dataframe(solution_df)
 
-#with tab3:
-#    st.write(ANSWER_STR)
+with tab3:
+    exercise_name = exercise.loc[0, "exercise_name"]
+    with open(f"answers/{exercise_name}.sql", "r") as f:
+        answer = f.read()
+    st.write(answer)
+
+
+
+
--- init_db.py
+++ init_db.py
@@ -1,3 +1,5 @@
+# pylint: disable = missing-module-docstring
+
 import io
 import pandas as pd
 import duckdb
@@ -21,21 +23,20 @@ con.execute("CREATE TABLE IF NOT EXISTS memory_state AS SELECT * FROM memory_sta
 # ------------------------------------------------------------
 # CROSS JOIN EXERCISES
 # ------------------------------------------------------------
-csv = """
+CSV = """
 beverage,price
 orange juice,2.5
 Expresso,2
 Tea,3
 """
-beverages = pd.read_csv(io.StringIO(csv))
+beverages = pd.read_csv(io.StringIO(CSV))
 con.execute("CREATE TABLE IF NOT EXISTS beverages AS SELECT * FROM beverages")
 
-csv2 = """
+CSV2 = """
 food_item,food_price
 cookie juice,2.5
 chocolatine,2
 muffin,3
 """
-food_items = pd.read_csv(io.StringIO(csv2))
+food_items = pd.read_csv(io.StringIO(CSV2))
 con.execute("CREATE TABLE IF NOT EXISTS food_items AS SELECT * FROM food_items")
-
